e97b5d57935bdfb9977039a89d456a3b08652b18
Harpo Roeder <hroeder@launchdarkly.com>
2019-11-30 21:18:28 -0800

    basic hello app

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,25 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
-import LaunchDarkly.Server
+import Control.Concurrent  (threadDelay)
+import Control.Monad       (forever)
+import Data.Text           (Text)
+
+import LaunchDarkly.Server (makeUser, makeConfig, makeClient, boolVariation)
+
+yourSDKKey :: Text
+yourSDKKey = "<put your SDK key here>"
+
+yourFlagKey :: Text
+yourFlagKey = "<put your feature key here>"
 
 main :: IO ()
-main = putStrLn "test"
+main = do
+    let user = makeUser "abc"
+    client <- makeClient $ makeConfig yourSDKKey
+    forever $ do
+        launched <- boolVariation client yourFlagKey user False
+        putStrLn $ "Flag is: " ++ show launched
+        -- one microsecond
+        threadDelay $ 1 * 1000000

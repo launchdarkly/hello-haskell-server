1179e66e45033ec0e98de6b6097b5f8719f0add5
hroederld <hroeder@launchdarkly.com>
2019-12-01 22:27:59 -0800

    [ch57667] initial app (#1)

---
+++ app/Main.hs
@@ -0,0 +1,25 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Main where
+
+import Control.Concurrent  (threadDelay)
+import Control.Monad       (forever)
+import Data.Text           (Text)
+
+import LaunchDarkly.Server (makeUser, makeConfig, makeClient, boolVariation)
+
+yourSDKKey :: Text
+yourSDKKey = "<put your SDK key here>"
+
+yourFlagKey :: Text
+yourFlagKey = "<put your feature key here>"
+
+main :: IO ()
+main = do
+    let user = makeUser "user@example.com"
+    client <- makeClient $ makeConfig yourSDKKey
+    forever $ do
+        launched <- boolVariation client yourFlagKey user False
+        putStrLn $ "Flag is: " ++ show launched
+        -- one second
+        threadDelay $ 1 * 1000000
